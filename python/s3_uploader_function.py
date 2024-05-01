13f4ad5d11a9d0073804e185467548fbdcb6c346
Harrison Sherwin <70494788+hasherwi@users.noreply.github.com>
2024-05-01 14:07:55 -0600

    S3 Uploader needs to signal CloudFormation back

--- python/s3_uploader_function.py
+++ python/s3_uploader_function.py
@@ -3,24 +3,44 @@
 import json
 import urllib.request
 import boto3
+from urllib import request
+
+def send_response(event, context, response_status, response_data):
+    """Send a signal back to Amazon CloudFormation."""
+    response_body = json.dumps({
+        "Status": response_status,
+        "Reason": "See the details in CloudWatch Log Stream: " + context.log_stream_name,
+        "PhysicalResourceId": context.log_stream_name,
+        "StackId": event['StackId'],
+        "RequestId": event['RequestId'],
+        "LogicalResourceId": event['LogicalResourceId'],
+        "Data": response_data
+    })
+
+    headers = {
+        'content-type': '',
+        'content-length': str(len(response_body))
+    }
+
+    response_request = request.Request(event['ResponseURL'], method='PUT', data=response_body.encode('utf-8'), headers=headers)
+    with request.urlopen(response_request) as response: 
+        print("Status code:", response.getcode())
+        print("Status message:", response.msg)
 
 def lambda_handler(event, context):
     """A simple Amazon CloudFormation helper function to upload files to Amazon S3."""
+    
+    try:
+        # Log the entire event and context objects
+        print("Event: ", json.dumps(event))
+        print("Context: ", context)
 
-    # Log the entire event and context objects
-    print("Event: ", json.dumps(event))
-        # Convert the event dictionary to a JSON formatted string and print
-    print("Context: ", context)
-        # The context object is not serializable to JSON directly without custom handling
+        s3 = boto3.client('s3')
+        bucket_name = event['ResourceProperties']['BucketName']
+        url = event['ResourceProperties']['HtmlFileUrl']
 
-    # Setup the S3 SDK client
-    s3 = boto3.client('s3')
-    bucket_name = event['ResourceProperties']['BucketName']
-
-    # Open the URL
-    url = event['ResourceProperties']['HtmlFileUrl']
-    with urllib.request.urlopen(url) as response:
-        data = response.read()
+        with urllib.request.urlopen(url) as response:
+            data = response.read()
 
         # Store the data in S3.
         s3.put_object(
@@ -29,5 +49,9 @@ def lambda_handler(event, context):
             Body=data,
             ContentType='text/html'
         )
-
-    return {'PhysicalResourceId': event.get('PhysicalResourceId', 'HtmlUploaderLambda')}
+        
+        send_response(event, context, "SUCCESS", {"Message": "Resource creation successful"})
+    
+    except Exception as e:
+        print("Failed to process:", e)
+        send_response(event, context, "FAILED", {"Message": str(e)})
